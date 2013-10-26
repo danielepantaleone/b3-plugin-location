c4b66327dd9d33f1a1ad97d9e614967e3636d3b7
FenixXx <fenix@urbanterror.info>
2013-10-27 00:36:33 +0200

    minor bugfixes and code cleanup

--- extplugins/location.py
+++ extplugins/location.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 __author__ = 'Fenix - http://www.urbanterror.info'
-__version__ = '1.0'
+__version__ = '1.1'
 
 import b3
 import b3.plugin
@@ -39,23 +39,22 @@ class LocationPlugin(b3.plugin.Plugin):
         
         try:
             self._announce = self.config.getboolean('settings', 'announce')
-            self.debug('Loaded announce setting: %r' % self._announce)
+            self.debug('loaded announce setting: %r' % self._announce)
         except Exception, e:
-            self.error('Could not load announce setting: %s' % e)
-            self.debug('Using default value for announce: %r' % self._announce)
-
+            self.error('could not load announce setting: %s' % e)
+            self.debug('using default value for announce: %r' % self._announce)
 
     def onStartup(self):
         """
         Initialize plugin settings
         """
-        # Get the admin plugin
+        # get the admin plugin
         self._adminPlugin = self.console.getPlugin('admin')
         if not self._adminPlugin:    
             self.error('Could not find admin plugin')
             return False
         
-        # Register our commands
+        # register our commands
         if 'commands' in self.config.sections():
             for cmd in self.config.options('commands'):
                 level = self.config.get('commands', cmd)
@@ -68,15 +67,13 @@ class LocationPlugin(b3.plugin.Plugin):
                 if func: 
                     self._adminPlugin.registerCommand(self, cmd, level, func, alias)
         
-        # Register the events needed
+        # register the events needed
         self.registerEvent(b3.events.EVT_CLIENT_CONNECT)
-
 
     # ######################################################################################### #
     # ##################################### HANDLE EVENTS ##################################### #        
     # ######################################################################################### #    
-     
-     
+
     def onEvent(self, event):
         """
         Handle intercepted events
@@ -84,71 +81,67 @@ class LocationPlugin(b3.plugin.Plugin):
         if event.type == b3.events.EVT_CLIENT_CONNECT:
             self.onConnect(event.client)
 
-
     # ######################################################################################### #
     # ####################################### FUNCTIONS ####################################### #        
     # ######################################################################################### # 
-    
-    
+
     def getCmd(self, cmd):
         cmd = 'cmd_%s' % cmd
         if hasattr(self, cmd):
             func = getattr(self, cmd)
             return func
         return None    
-    
-    
+
     def getLocationData(self, client):
         """
         Retrieve location data from the API
         """
         try:    
             
-            # Will retrieve necessary data from the API and perform some checks on it
-            self.debug("Contacting http://ip-api.com to retrieve location data for %s..." % client.name)
-            data = json.load(urllib2.urlopen('http://ip-api.com/json/%s' % (str(client.ip))))  
+            # will retrieve necessary data from the API and perform some checks on it
+            self.debug("contacting http://ip-api.com to retrieve location data for %s..." % client.name)
+            data = json.load(urllib2.urlopen('http://ip-api.com/json/%s' % client.ip))
         
         except urllib2.URLError, e:
-            self.warning("Could not connect to http://ip-api.com: %s" % e)
+            self.warning("could not connect to http://ip-api.com: %s" % e)
             return None
             
         if data['status'] == 'fail':
-            self.debug('Could not retrieve valid geolocation info using ip %s: %s' % (str(client.ip), data['message']))
+            self.debug('could not retrieve valid geolocation info using ip %s: %s' % (client.ip, data['message']))
             return None
         
         if 'country' not in data:
-            self.debug('Could not establish in which country is ip %s' % str(client.ip))
+            self.debug('could not establish in which country is ip %s' % client.ip)
             return None
         
-        self.debug("Retrieved location data for %s: %r" % (client.name, data))
+        self.debug("retrieved location data for %s: %r" % (client.name, data))
         return data
-    
-    
-    def getLocationDistance(self, client, sclient):
+
+    def getLocationDistance(self, cl1, cl2):
         """
         Return the distance between 2 clients (in Km)
         """
-        if not client.isvar(self, 'location'):
-            self.debug('Could not compute distance: %s has no location data' % client.name)
+        if not cl1.isvar(self, 'location'):
+            self.debug('could not compute distance: %s has no location data' % cl1.name)
             return False
         
-        if not sclient.isvar(self, 'location'):
-            self.debug('Could not compute distance: %s has no location data' % sclient.name)
+        if not cl2.isvar(self, 'location'):
+            self.debug('could not compute distance: %s has no location data' % cl2.name)
             return False
 
-        location1 = client.var(self, 'location').value
-        location2 = sclient.var(self, 'location').value
+        location1 = cl1.var(self, 'location').value
+        location2 = cl2.var(self, 'location').value
         
         if not 'lat' in location1 or not 'lon' in location1:
-            self.debug('Could not compute distance: %s has no enough location parameters: %r' % (client.name, location1))
+            self.debug('could not compute distance: %s has no enough location parameters: %r' % (cl1.name, location1))
             return False
            
         if not 'lat' in location2 or not 'lon' in location2:
-            self.debug('Could not compute distance: %s has no enough location parameters: %r' % (sclient.name, location2))
+            self.debug('could not compute distance: %s has no enough location parameters: %r' % (cl2.name, location2))
             return False
         
-        # Print some verbose logging just for testing purpose
-        self.verbose('Computing distance between %s and %s' % (client.name, sclient.name))
+        # print some verbose logging just for testing purpose
+        self.verbose('computing distance between %s and %s' % (cl1.name, cl2.name))
         
         lat1 = float(location1['lat'])
         lat2 = float(location2['lat'])
@@ -159,7 +152,7 @@ class LocationPlugin(b3.plugin.Plugin):
         # Haversine formula
         ###
         
-        radius = 6371 # Earth radius in Km
+        radius = 6371  # Earth radius in Km
         dlat = math.radians(lat2 - lat1)
         dlon = math.radians(lon2 - lon1)
         
@@ -169,44 +162,47 @@ class LocationPlugin(b3.plugin.Plugin):
         b = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
         return round(abs(radius * b), 2)
-    
-        
+
     def onConnect(self, client):
         """
         Handle EVT_CLIENT_CONNECT
         """
-        location = self.getLocationData(client)
+        loc = self.getLocationData(client)
         
-        if not location:
+        if not loc:
             
             if self._announce and self.console.upTime() > 300:
-                # Since we have to announce a connection inform that
+                # since we have to announce a connection inform that
                 # a guy from an unknown location connected to the server
                 self.console.say('^7%s ^3from ^7-- ^3connected' % client.name)
             
             return
-        
-        # Store data in the client object so we do
+
+        # trim off whitespaces since api sometime
+        # returns data without trimming them
+        loc['country'] = loc['country'].strip()
+        if 'city' in loc:
+            loc['city'] = loc['city'].strip()
+
+        # store data in the client object so we do
         # not have to query the API on every request
-        client.setvar(self, 'location', location)
+        client.setvar(self, 'location', loc)
         
-        # If we have to announce and we got a valid response
+        # if we have to announce and we got a valid response
         # from the API, print location info in the game chat        
         if self._announce and self.console.upTime() > 300:
-            
-            message = '^7%s ^3from ^7%s ^3connected' % (client.name, location['country'])
-            if 'city' in location:
-                # If we got a city overwrite previously generated message with a more detailed one
-                message = '^7%s ^3from ^7%s ^3(^7%s^3) connected' % (client.name, location['city'], location['country'])  
+
+            message = '^7%s ^3from ^7%s ^3connected' % (client.name, loc['country'])
+            if 'city' in loc:
+                # if we got a city overwrite previously generated message with a more detailed one
+                message = '^7%s ^3from ^7%s ^3(^7%s^3) connected' % (client.name, loc['city'], loc['country'])
                 
             self.console.say(message)
-        
 
     # ######################################################################################### #
     # ######################################## COMMANDS ####################################### #        
     # ######################################################################################### # 
-    
-     
+
     def cmd_locate(self, data, client, cmd=None):
         """\
         <client> - Display geolocation info of the specified client
@@ -215,28 +211,27 @@ class LocationPlugin(b3.plugin.Plugin):
             client.message('^7Invalid data, try ^3!^7help locate')
             return
         
-        sclient = self._adminPlugin.findClientPrompt(data, client)
-        if not sclient: 
+        cl = self._adminPlugin.findClientPrompt(data, client)
+        if not cl:
             return
 
-        if not sclient.isvar(self, 'location'):
-            cmd.sayLoudOrPM(client, '^7Could not locate ^1%s' % sclient.name)
+        if not cl.isvar(self, 'location'):
+            cmd.sayLoudOrPM(client, '^7Could not locate ^1%s' % cl.name)
             return 
         
-        # Get the client location data
-        location = sclient.var(self, 'location').value
+        # get the client location data
+        loc = cl.var(self, 'location').value
         
-        message = '^7%s ^3is connected from ^7%s' % (sclient.name, location['country'])     
-        if 'city' in location:
-            # If we got a city overwrite previously generated message with a more detailed one
-            message = '^7%s ^3is connected from ^7%s ^3(^7%s^3)' % (sclient.name, location['city'], location['country'])   
+        message = '^7%s ^3is connected from ^7%s' % (cl.name, loc['country'])
+        if 'city' in loc:
+            # if we got a city overwrite previously generated message with a more detailed one
+            message = '^7%s ^3is connected from ^7%s ^3(^7%s^3)' % (cl.name, loc['city'], loc['country'])
         
         cmd.sayLoudOrPM(client, message)
         
-        if 'isp' in location:
-            # Display also the ISP of the client if we got a valid entry
-            cmd.sayLoudOrPM(client, '^3He\'s currently using ^7%s ^3as ISP' % location['isp'])
-        
+        if 'isp' in loc:
+            # sisplay also the ISP of the client if we got a valid entry
+            cmd.sayLoudOrPM(client, '^3He\'s currently using ^7%s ^3as ISP' % loc['isp'])
     
     def cmd_distance(self, data, client, cmd=None):
         """\
@@ -246,20 +241,19 @@ class LocationPlugin(b3.plugin.Plugin):
             client.message('^7Invalid data, try ^3!^7help distance')
             return
         
-        sclient = self._adminPlugin.findClientPrompt(data, client)
-        if not sclient: 
+        cl = self._adminPlugin.findClientPrompt(data, client)
+        if not cl:
             return
         
-        if sclient == client:
+        if cl == client:
             cmd.sayLoudOrPM(client, '^7Sorry, I\'m not that smart...meh!')
             return
         
-        # Compute the distance between the given clients
-        # This will return false in case we have data inconsistency
-        distance = self.getLocationDistance(client, sclient)
-        if distance == False:
-            cmd.sayLoudOrPM(client, '^7Could not compute distance with ^1%s' % sclient.name)
+        # compute the distance between the given clients
+        # this will return false in case we have data inconsistency
+        distance = self.getLocationDistance(client, cl)
+        if not distance:
+            cmd.sayLoudOrPM(client, '^7Could not compute distance with ^1%s' % cl.name)
             return
         
-        cmd.sayLoudOrPM(client, '^7%s ^3is ^7%.2f ^3km away from you' % (sclient.name, distance))
-        
+        cmd.sayLoudOrPM(client, '^7%s ^3is ^7%.2f ^3km away from you' % (cl.name, distance))
